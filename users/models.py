8315405269b857940d0dbcb7f3177c11f6b3fd7a
binja <buzzzzx233@gmail.com>
2017-09-30 15:24:39 +0800

    first commit.设计了user app的model UserProfile

---
+++ users/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+
+class UserProfile(AbstractUser):
+    nick_name = models.CharField(max_length=50, verbose_name=u"昵称", default="")
+    birthday = models.DateField(verbose_name=u"生日", null=True, blank=True)
+    gender = models.CharField(max_length=5, choices=(("male", u"男"), ("female", u"女")), default="female")
+    address = models.CharField(max_length=100, default=u"", verbose_name=u"地址")
+    mobile = models.CharField(max_length=11, null=True, blank=True)
+    image = models.ImageField(upload_to="image/%Y/%m", default=u"image/default.png", max_length=100)
+
+    class Meta:
+        verbose_name = "用户信息"
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.username
