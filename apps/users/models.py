f4c965cc8240a44618d984462f0ca76ef803a3b5
binja <buzzzzx233@gmail.com>
2017-10-03 20:15:37 +0800

    finish the users app register

--- apps/users/models.py
+++ apps/users/models.py
@@ -25,12 +25,15 @@ class UserProfile(AbstractUser):
 class EmailVerifyRecord(models.Model):
     code = models.CharField(max_length=20, verbose_name=u"验证码")
     email = models.EmailField(max_length=50, verbose_name=u"邮箱")
-    send_type = models.CharField(max_length=20, choices=(('register', u"注册"), ('forget', u"找回密码")))
-    send_time = models.DateTimeField(default=datetime.now) # 把括号去掉，这个时间就是这个class实例化的时间
+    send_type = models.CharField(verbose_name="验证码类型", max_length=20, choices=(('register', u"注册"), ('forget', u"找回密码")))
+    send_time = models.DateTimeField(default=datetime.now, verbose_name="发送时间")  # 把括号去掉，这个时间就是这个class实例化的时间
 
     class Meta:
         verbose_name = u"邮箱验证码"
         verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return '{0}({1})'.format(self.code, self.email)
 
 
 class Banner(models.Model):
